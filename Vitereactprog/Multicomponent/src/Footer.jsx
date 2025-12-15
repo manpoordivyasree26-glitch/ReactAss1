cef30d77cd22e5e2372f122084f94b766e0b7209
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2025-12-15 22:56:23 +0530

    component

---
+++ Vitereactprog/Multicomponent/src/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer(){
+    return(
+        <div
+        style={{
+          fontSize:"15px",
+          color:"brown",
+          backgroundColor:"greenyellow",
+          padding:"20px",
+          textAlign:"center"
+
+        }}
+        >This is Footer</div>
+    );
+}
+export default Footer;
---
+++ Vitereactprog/Multicomponent/src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <div
+        style={{
+            backgroundColor:"violet",
+            padding:"20px",
+            textAlign:"center",
+            color:"blue",
+            fontSize:"20px"
+
+
+        }}
+        >This is Navbar</div>
+    );
+}
+export default Navbar;
