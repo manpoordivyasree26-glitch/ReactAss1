0a4b342d1832c69cc54b5e7e8dcff9945359eb6f
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2025-12-15 18:22:06 +0530

    vite

---
+++ Vitereactprog/vite/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import FirstComponent from "./FirstComponent";
+
+function App(){
+  return(
+    <div>
+      <FirstComponent/>
+    </div>
+  );
+}
+export default App;
